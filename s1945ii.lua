f7109ec0197375919230456f7f5a0f2bd9194170
Chang-young Koh <lucent@LucentLaboratory2.local>
2016-03-13 12:33:03 +0900

    Fix #1

--- s1945ii.lua
+++ s1945ii.lua
@@ -14,6 +14,7 @@ function s1945ii.cheat()
     mem:write_u8(0x60103FA, 1)
 -- set P1 infinite life
     mem:write_u8(0x60103c1, 3)
+end
 
 function s1945ii.is_p1_invincible()
     return mem:write_u8(0x60103FA)
